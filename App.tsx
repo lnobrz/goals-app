060c06e602e73aa6c098707310fd4058ec46f81e
Lucas Nobre <lucasnobre37@outlook.com>
2023-08-10 01:54:20 -0300

    add Home page to App.tsx file

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet, ScrollView } from "react-native";
+import GoalsContextProvider from "./store/context/goals/GoalsContext";
+import Home from "./Pages/Home";
+import ExpoStatusBar from "expo-status-bar/build/ExpoStatusBar";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GoalsContextProvider>
+      <View style={styles.container}>
+        <ExpoStatusBar style="light" />
+        <Home />
+      </View>
+    </GoalsContextProvider>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    color: "fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    padding: 30,
+    paddingTop: 50,
   },
 });
